ed574cb4a131dfd3690a0fc419c3207631679099
Nilardri2006 <pramanicknilardri@gmail.com>
2025-08-31 10:43:43 +0530

    built

--- Paste App/vite.config.js
+++ Paste App/vite.config.js
@@ -1,7 +1,19 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vite.dev/config/
+// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  server: {
+    port: 3000,
+  },
+  build: {
+    outDir: 'dist'
+  },
+  resolve: {
+    alias: {
+      '@': '/src',
+    },
+  },
+  base: './',  // 👈 IMPORTANT for deployment
 })
